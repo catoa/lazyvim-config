448093f7aee6f4cf28b34c817a9029120ce7a74c
Anthony Cato <catoa0608@gmail.com>
2023-11-28 10:26:33 -0500

    add keymap for window resizing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+keymap.set("n", "<M-Left>", "<cmd>vertical resize -2<cr>")
+keymap.set("n", "<M-Right>", "<cmd>vertical resize +2<cr>")
+keymap.set("n", "<M-Up>", "<cmd>horizontal resize +2<cr>")
+keymap.set("n", "<M-Down>", "<cmd>horizontal resize -2<cr>")
