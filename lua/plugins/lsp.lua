004422cac32d43230e974a40fcf25c8bb99b49e2
Anthony Cato <anthony.cato@dutchie.com>
2024-03-08 21:55:49 -0500

    update settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,20 @@ return {
   { "rust-lang/rust.vim" },
   { "cappyzawa/starlark.vim" },
   {
+    "topaxi/gh-actions.nvim",
+    cmd = "GhActions",
+    keys = {
+      { "<leader>ga", "<cmd>GhActions<cr>", desc = "Open Github Actions" },
+    },
+    -- optional, you can also install and use `yq` instead.
+    build = "make",
+    dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+    opts = {},
+    config = function(_, opts)
+      require("gh-actions").setup(opts)
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     dependencies = {
       "jose-elias-alvarez/typescript.nvim",
