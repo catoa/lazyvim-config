5f1813c6e98b73a739934a25f52ccb8d0e3ce3d4
Anthony Cato <catoa0608@gmail.com>
2024-04-17 03:04:57 -0400

    try this

--- cli.py
+++ cli.py
@@ -4,7 +4,7 @@ import json
 # @app.command()
 def thistest():
     x = input()
-    print(json.loads(x))
+    print(x)
     # pr_body = json.loads(pr_body)
     # typer.echo(f"Hello {pr_body['action']}")
 
