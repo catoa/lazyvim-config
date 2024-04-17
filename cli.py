13a4b8dd2e1688ffd43d5e4585dc310ba294cf6f
Anthony Cato <catoa0608@gmail.com>
2024-04-17 03:41:10 -0400

    try this

--- cli.py
+++ cli.py
@@ -5,8 +5,8 @@ PR_SECTION_HEADER = "## Pull Request Branches"
 
 def main():
     lines = [line.strip() for line in sys.stdin.readlines() if line.strip()]
-    pr_section = lines[lines.index(PR_SECTION_HEADER) :]
-    print(pr_section)
+    branches = lines[lines.index(PR_SECTION_HEADER) + 1 :]
+    print([branch[1:].split("-") for branch in branches])
 
 
 if __name__ == "__main__":
