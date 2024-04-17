61c7e9ad58ffc2afaceb8dc230ef68999e3a1ee9
Anthony Cato <catoa0608@gmail.com>
2024-04-17 09:42:50 -0400

    try this

--- cli.py
+++ cli.py
@@ -1,6 +1,9 @@
 import sys
 from dataclasses import dataclass
 
+
+from typing import Self
+
 PR_SECTION_HEADER = "## Pull Request Branches"
 
 
@@ -10,18 +13,19 @@ class PullRequestEnvBranch:
     branch: str
     selected: bool
 
+    @classmethod
+    def from_line(cls, line) -> Self:
+        app_name, branch = tuple(line.replace("-", "", 1).strip().split(":"))
+        selected, app_name = app_name.split()
+        return cls(app_name, branch.strip(), selected == "[x]".lower())
+
 
 def main():
     lines = [line.strip() for line in sys.stdin.readlines() if line.strip()]
     branches = lines[lines.index(PR_SECTION_HEADER) + 1 :]
     print(f"Branches: {branches}")
 
-    def parse_repo_and_branch_name(branch):
-        app_name, branch = tuple(branch.replace("-", "", 1).strip().split(":"))
-        selected, app_name = app_name.split()
-        return PullRequestEnvBranch(app_name, branch.strip(), selected == "[x]".lower())
-
-    print([parse_repo_and_branch_name(branch) for branch in branches])
+    print([PullRequestEnvBranch.from_line(branch) for branch in branches])
 
 
 if __name__ == "__main__":
