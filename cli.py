5720b633210bd418fa65365d610bf82b14966f6d
Anthony Cato <catoa0608@gmail.com>
2024-04-17 03:46:53 -0400

    try this

--- cli.py
+++ cli.py
@@ -7,7 +7,7 @@ def main():
     lines = [line.strip() for line in sys.stdin.readlines() if line.strip()]
     branches = lines[lines.index(PR_SECTION_HEADER) + 1 :]
     print(f"Branches: {branches}")
-    print([branch.replace("-", 1) for branch in branches])
+    print([branch.replace("-", "", 1) for branch in branches])
 
 
 if __name__ == "__main__":
