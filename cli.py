50369dbba9e5bb3565ea982798b0852613544e5e
Anthony Cato <catoa0608@gmail.com>
2024-04-17 03:34:42 -0400

    try this

--- cli.py
+++ cli.py
@@ -4,6 +4,8 @@ import sys
 # @app.command()
 def thistest():
     x = [line.strip() for line in sys.stdin.readlines() if line.strip()]
+    pr_section = x[x.index("## Pull Request Branches") :]
+    print(pr_section)
     # x = input()
     print(x)
     # pr_body = json.loads(pr_body)
